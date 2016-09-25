f757b7169e6b615b642c81f1793fffdaf0580523
EchoXiao <EchoXiao@users.noreply.github.com>
2016-09-26 06:00:02 +0800

    Update and rename test.py to test_code_1.py

--- test_code_1.py
+++ test_code_1.py
@@ -1,4 +1,3 @@
-# Data Analysis with python
 
 # Question 1
 # Given a game with device id and payment on each day, and given the data on three days, ask to calculate all the payment for each device in these three days.
