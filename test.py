aa7c83e74766fd0d3e055f096953e39b65a68a28
EchoXiao <EchoXiao@users.noreply.github.com>
2016-09-26 05:57:22 +0800

    Update test.py

--- test.py
+++ test.py
@@ -8,11 +8,11 @@ import pandas as pd
 
 
 s1 = pd.DataFrame({"device_id": ["00001", "00003", "00009"], 
-					"payment": [20.20, 4.99, 12.5]})
+				"payment": [20.20, 4.99, 12.5]})
 s2 = pd.DataFrame({"device_id": ["00001", "00003", "00009"], 
-					"payment": [0, 55.00, 6.50]})
+				"payment": [0, 55.00, 6.50]})
 s3 = pd.DataFrame({"device_id": ["00001", "00003", "00009"], 
-					"payment": [9.99, 29.99, 0]})
+				"payment": [9.99, 29.99, 0]})
 
 result = pd.concat([s1, s1, s3])
 result.sum("payment")
@@ -21,17 +21,19 @@ grouped.sum()
 
 # --------------------------------------------------------------------------------------
 # Question 2
-# Suppose on the day of 11/25, players have made none, one, or more payment transactions, ask to provice the total payment of each player when total payment is more than $20 and user name is not "internal_employee".
+# Suppose on the day of 11/25, players have made none, one, or more payment transactions,
+# ask to provice the total payment of each player when total payment is more than $20 and
+# user name is not "internal_employee".
 
 
 
 df = pd.DataFrame({"user_name": 
-					["Andy", "Laurie", "Ken", "Andy", "Andy", "Laurie", 
-					"Frank", "Laurie", "Ken", "Internal_employee"],
-					"transaction_id":
-					[18, 77, 55, 88, 118, 102, 217, 389, 270, 215],
-					"amount": 
-					[9.99, 4.99, 4.99, 19.99, 4.99, 9.99, 9.99, 9.99, 4.99, 29.99]})
+		["Andy", "Laurie", "Ken", "Andy", "Andy", "Laurie", 
+		 "Frank", "Laurie", "Ken", "Internal_employee"],
+		"transaction_id":
+		[18, 77, 55, 88, 118, 102, 217, 389, 270, 215],
+		"amount": 
+		[9.99, 4.99, 4.99, 19.99, 4.99, 9.99, 9.99, 9.99, 4.99, 29.99]})
 df = df.drop([9]) # delet "internal_employee"
 df = df.groupby("user_name").sum()
 df[df["amount"] > 20]
